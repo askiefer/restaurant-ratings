10a10dced2edf1fc6e29ba0979c85e1a1c9feba4
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 17:59:32 -0700

    use dictionary

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -2,28 +2,52 @@ import sys
 
 filename = sys.argv[1]
 
+from random import choice
+
 def print_restaurant_ratings(filename):
     """This prints restaurant ratings from a file."""
 
     with open(filename) as in_file:
+        # asks user for their name
+        user_name = raw_input("Hello! What is your name? ")
 
-        restaurants = []
+        #initializes a list and asks user to add a restaurant and score to it
+        restaurants = {}
+        #restaurant_name = raw_input("What restaurant would you like to review? ")
+        #restaurant_score = raw_input("What score do you give this restaurant? ")
+        #restaurants.append((restaurant_name, int(restaurant_score)))
 
         # iterates through each line and splits at colon then inputs it into dict
-        
-        for line in in_file: 
+        for line in in_file:
             # gets rid of newline character at end of line
             line = line.rstrip()
             # makes a list from file line: ["Restaurant Name", score]
             restaurant_and_rating = line.split(":")
             # adds a tuple of name and rating to a list of all restaurants
-            restaurants.append((restaurant_and_rating[0], restaurant_and_rating[1]))
+            restaurants[restaurant_and_rating[0]] = restaurant_and_rating[1]
         
+        # chooses a random restaurant and unpacks name and score from tuple
+        # random_restaurant = choice(restaurants)
+        # rand_rest_name = random_restaurant[0]
+        # rand_rest_score = random_restaurant[1]
+
+        # # tells user current rating and asks for a new one
+        # print "Hello, %s! %s has a rating of %s." %(user_name, rand_rest_name, rand_rest_score)
+        # new_score = int(raw_input("What rating would you give %s?" %(rand_rest_name)))
+
+        # # updates restaurant rating
+        # for item in restaurants:
+        #     # Item[0] is restaurant tuple, item[0][0] is restaurant name, item[0][1] is score
+        #     if item[0] == rand_rest_name:
+        #         del restaurants[restaurants.index(item)]
+
+        # restaurants.append((rand_rest_name, new_score))
+
         # sorts list alphabetically
-        restaurants = sorted(restaurants)
+        rest_list = sorted(restaurants)
         # prints each tuple of name and rating on a new line
-        for item in restaurants:
-            print item[0], "has a rating of:", item[1]
+        for key in rest_list:
+            print key, "has a rating of:", restaurants[key]
 
 
 
